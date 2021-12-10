631e089d048b0be313f4fcb6577b01c78563eca5
guillerp <72713057+guillerp8@users.noreply.github.com>
2021-12-11 00:26:52 +0100

    Update Points.lua

--- Client/Points.lua
+++ Client/Points.lua
@@ -113,8 +113,9 @@ function openBossMenu()
     end
     local cb = "member"
     for k, v in pairs(gangData.members) do
-        --
-        table.insert(data, {text = v.member.name.. " - " ..v.member.rank, toDo = v.member.steam})
+        if v.member.name and v.member.steam then
+            table.insert(data, {text = v.member.name.. " - " ..v.member.rank, toDo = v.member.steam})
+        end
     end
     TriggerEvent("guille_cont:client:open", _U("click_to_ex"), data, cb, false)
 end
